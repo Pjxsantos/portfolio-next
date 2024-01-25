1efa56e5ff07c9478f985f9861bb3640dc77b572
Pjxsantos <amadora2405@gmail.com>
2024-01-25 20:24:38 -0300

    atual

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -52,7 +52,7 @@ const AboutSection = () => {
 
   return (
     <section className="text-white" id="about">
-      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center sm:border-[#33353F] sm:border rounded-md  py-8 px-16 flex flex-col sm:flex-row xl:gap-16 sm:py-16 xl:px-16">
         <Image src="/images/matrix.jpeg" width={500} height={500} />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">UM POUCO S​OBRE MIM​ !</h2>
